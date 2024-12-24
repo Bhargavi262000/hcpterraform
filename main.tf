7a6f87d25fc70582b0cc0277e409007294a596d3
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-24 11:30:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,23 @@ provider "google" {
   project = "hcp-tf-test-bhargavi"
   region  = "us-central1"
 }
+
+resource "google_storage_bucket" "test_bucket_good" {
+  name                        = "test-bucket-good"
+  location                    = "EU"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
+resource "google_container_node_pool" "good_node_pool" {
+  name               = "good-node-pool"
+  cluster            = "cluster-1"
+  project = "hcp-tf-test-bhargavi"
+  location           = "US"
+  initial_node_count = 3
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+  }
+}
