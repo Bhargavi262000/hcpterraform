6f7dafd282bbd7de862abc383fe08a6a058d1383
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-25 21:22:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,25 @@ resource "google_storage_bucket" "test_bucket_good" {
   uniform_bucket_level_access = true
 }
 
-resource "google_container_node_pool" "good_node_pool" {
-  name               = "good-node-pool"
-  cluster            = "cluster-1"
-  project = "hcp-tf-test-bhargavi"
-  location           = "EU"
-  initial_node_count = 3
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
+resource "google_securityposture_posture" "posture_2" {
+  posture_id  = "posture_2"
+  parent      = "organizations/714470867684"
+  location    = "global"
+  state       = "ACTIVE"
+  description = "a new posture"
+  policy_sets {
+      policy_set_id = "org_policy_set"
+      description   = "set of org policies"
+      policies {
+          policy_id = "policy_1"
+          constraint {
+              org_policy_constraint {
+                  canned_constraint_id = "storage.uniformBucketLevelAccess"
+                  policy_rules {
+                      enforce = true
+                  }
+              }
+          }
+      }
   }
 }
