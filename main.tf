6d25e1d38cae4309e9c84dda147ae20a2b7c57a0
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-26 19:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "google" {
   region  = "us-central1"
 }
 
+resource "google_storage_bucket" "test_bucket" {
+  count                       = 1024
+  name                        = var.test_bucket_names[count.index]
+  location                    = "US"
+  storage_class               = "STANDARD"
+  project                     = "hcp-tf-test-bhargavi"
+  uniform_bucket_level_access = true
+}
+
