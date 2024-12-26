c597fc5652dd2563b762165d4cccd650bcf255cb
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-26 17:54:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ provider "google" {
   project = "hcp-tf-test-bhargavi"
   region  = "us-central1"
 }
+resource "google_folder" "testfolderbhargavi" {
+  display_name = "policy"
+  parent       = "organizations/714470867684"
+}
