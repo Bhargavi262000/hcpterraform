202ee7bd4948c52eabafca5d9b883bb599f3c56c
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-26 17:50:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,3 @@ provider "google" {
   project = "hcp-tf-test-bhargavi"
   region  = "us-central1"
 }
-
-resource "google_storage_bucket" "non_violating_bucket" {
-  name     = "non_violating_bucket" # Replace with a unique name
-  location = "us-central1"          # Replace with desired region
-  project  = "hcp-tf-test-bhargavi"
-}
-
