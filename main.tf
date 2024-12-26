72e7797faf409cfa8c25d9926d71079f7241b38c
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-26 18:57:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,3 @@ provider "google" {
   project = "hcp-tf-test-bhargavi"
   region  = "us-central1"
 }
-resource "google_folder" "testfolderbhargavi" {
-  display_name = "policy"
-  parent       = "organizations/714470867684"
-}
