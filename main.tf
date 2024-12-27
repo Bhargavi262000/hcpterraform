4cfb23ecf7d4b891a1566ea410f2852d21f14d42
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-27 08:53:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ provider "google" {
   project = "hcp-tf-test-bhargavi"
   region  = "us-central1"
 }
+
+resource "google_compute_network" "network1"{
+  name                                        = "network-vpc1"
+  delete_default_routes_on_create = false
+  auto_create_subnetworks           = false
+  routing_mode                      = "REGIONAL"
+  mtu = 100
+  project = "hcp-tf-test-bhargavi"
+}
