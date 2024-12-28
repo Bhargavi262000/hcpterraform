a649eb5bfa1bae24fb6378ef1cc1e3edeea4e3b0
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-28 06:11:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,3 @@ resource "google_folder" "folder" {
   display_name = "policy"
   parent       = "organizations/1000043173737"
 }
-
-
-resource "google_org_policy_policy" "primary" {
- name   = "projects/461255015551/policies/compute.requireOsLogin"
- parent = "projects/461255015551"
-
-
- spec {
-   rules {
-     enforce = "FALSE"
-   }
- }
-}
