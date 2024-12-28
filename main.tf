ca35acc9593c1d2d72f08728d75578a53f5cbc57
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-28 06:10:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,50 +3,20 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_compute_network" "network1"{
-  name                                        = "network-vpc1"
-  delete_default_routes_on_create = false
-  auto_create_subnetworks           = false
-  routing_mode                      = "REGIONAL"
-  mtu = 100
-  project = "scenario-8-20assetsviolation"
-}
-
-module "gcp_org_policy_v2" {
- source           = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
-
- policy_root      = "organization"
- policy_root_id   = "1000043173737"
- constraint       = "iam.disableServiceAccountKeyUpload"
- policy_type      = "boolean"
- exclude_folders  = []
- exclude_projects = []
-
-
- rules = [
-   # Rule 1
-   {
-    // Modification(enforcement - false ->true).
-     enforcement = true
-     allow       = []
-     deny        = []
-     conditions  = []
-   },
- ]
+resource "google_folder" "folder" {
+  display_name = "policy"
+  parent       = "organizations/1000043173737"
 }
 
 
-// Resource already exists.
 resource "google_org_policy_policy" "primary" {
- name   = "projects/55476130589/policies/compute.requireOsLogin"
- parent = "projects/55476130589"
+ name   = "projects/461255015551/policies/compute.requireOsLogin"
+ parent = "projects/461255015551"
 
 
  spec {
    rules {
-    // Modification(enforcement - TRUE -> FALSE).
      enforce = "FALSE"
    }
  }
 }
-
