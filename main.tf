7801ff99ceb875696920a108daa00c8785c89f95
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2024-12-30 12:33:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,30 +3,7 @@ provider "google" {
   region  = "us-central1"
 }
 
-# New folder, child of existing org.
-resource "google_folder" "scenario_9" {
-  display_name = "Scenario 9"
-  parent       = "organizations/714470867684"
-}
-
-# New folder, child of new folder.
-resource "google_folder" "child_folder" {
-  display_name = "Child Folder"
-  parent       = google_folder.scenario_9.name
-}
-
-# New Project, child of new folder.
-resource "google_project" "new_project_in_new_folder" {
-  name       = "New Project in New Folder"
-  project_id = "new-project-in-new-folder"
-  folder_id  = google_folder.child_folder.name
-}
-
-# New bucket, child of new project
-resource "google_storage_bucket" "new_bucket" {
-  name                        = "new_bucket"
-  location                    = "US"
-  storage_class               = "STANDARD"
-  project                     = google_project.new_project_in_new_folder.project_id
-  uniform_bucket_level_access = true
+resource "google_folder" "folder" {
+  display_name = "policy"
+  parent       = "organizations/1000043173737"
 }
