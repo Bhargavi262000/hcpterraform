c5623421c5c9894ccaab805ea1fdfa996798ee9a
Bhargavi262000 <72136568+Bhargavi262000@users.noreply.github.com>
2025-01-01 20:51:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,41 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_folder" "folder" {
-  display_name = "policy"
-  parent       = "organizations/1000043173737"
+// Resource already exists.
+module "gcp_org_policy_v2" {
+ source           = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+
+ policy_root      = "organization"
+ policy_root_id   = "1000043173737"
+ constraint       = "iam.disableServiceAccountKeyUpload"
+ policy_type      = "boolean"
+ exclude_folders  = []
+ exclude_projects = []
+
+
+ rules = [
+   # Rule 1
+   {
+    // Modification(enforcement - false ->true).
+     enforcement = true
+     allow       = []
+     deny        = []
+     conditions  = []
+   },
+ ]
+}
+
+
+// Resource already exists.
+resource "google_org_policy_policy" "primary" {
+ name   = "projects/55476130589/policies/compute.requireOsLogin"
+ parent = "projects/55476130589"
+
+
+ spec {
+   rules {
+    // Modification(enforcement - TRUE -> FALSE).
+     enforce = "FALSE"
+   }
+ }
 }
